7149030d213b51e9e027b7ec14cc1f3be4d2a6b2
midpoint <midpoint2005@gmail.com>
2024-09-26 19:54:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ ZHOYAN_API_KEY=os.environ.get("ZHOYAN_API_KEY")
 
 if __name__ == "__main__":
     weather = get_weather(CAIYUN_KEY,LOCATION)
-    print(weather)
+    # print(weather)
     text=f'# 每日早报\n---\n## {CITY}天气\n'
     text=text+str(weather)
     sentence1=get_one_sentence(SENTENCE_API,SENTENCE_TOKEN)
-    print('sentence1:',sentence1)
-    # picurl=make_pic(zhoyan_api_key,sentence1)
-    picurl="https://sfile.chatglm.cn/testpath/be86d380-ee41-5aa9-a96d-2775cd12cd0e_0.png"
+    # print('sentence1:',sentence1)
+    picurl=make_pic(zhoyan_api_key,sentence1)
+    # picurl="https://sfile.chatglm.cn/testpath/be86d380-ee41-5aa9-a96d-2775cd12cd0e_0.png"
     print('picurl:',picurl)
     text=text+f'\n---\n## 每日一图\n{sentence1}\n'
     text=text+f'![{sentence1}]({picurl})'   #!['+sentence1+']('+picurl+')'
-    print(text)
+    # print(text)
 
     send_dd(DINGTALK_WEBHOOK,DD_SIGN,text)
-    # send_tg(telegram_bot_token,telegram_chat_id, sentence1,picurl)
+    send_tg(telegram_bot_token,telegram_chat_id, sentence1,picurl)
