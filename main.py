2f52c3ff7cf84301e63283be3f020693f913bc50
midpoint <midpoint2005@gmail.com>
2024-10-15 11:23:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,17 @@ ISSUE_NUMBER = os.environ.get("ISSUE_NUMBER")
 
 if __name__ == "__main__":
     weather = get_weather(CAIYUN_KEY,LOCATION)
-    # print(weather)
-    text=f'# 每日早报\n- {get_day()}\n---\n## 今日天气[{CITY}]\n'
+    
+    text=f'# 每日早报\n---\n- {get_day()}\n---\n## 今日天气[{CITY}]\n'
     text=text+str(weather)
     sentence1=get_one_sentence(SENTENCE_API,SENTENCE_TOKEN)
-    # print('sentence1:',sentence1)
+    
     picurl=make_pic(ZHOYAN_API_KEY,sentence1)
-    # picurl="https://sfile.chatglm.cn/testpath/be86d380-ee41-5aa9-a96d-2775cd12cd0e_0.png"
-    print('picurl:',picurl)
     text=text+f'\n---\n## 一诗一图\n{sentence1}\n'
-    text=text+f'![]({picurl})'   #!['+sentence1+']('+picurl+')'
-    # print(text)
+    text=text+f'![sentence1]({picurl})'   #!['+sentence1+']('+picurl+')'
+
+    text=text+f'\n---\n## 热点新闻 \n{World_60S()}'
+    
     create_comment(G_T,REPO_NAME,ISSUE_NUMBER,text)
 
     send_dd(DINGTALK_WEBHOOK,DD_SIGN,text)
